29e54deb85920150e1138504c3fd8a5aa589229a
juan salamanca <juanmsalamancatec@gmail.com>
2018-10-28 18:14:32 -0500

    up

--- app/src/main/java/com/lastfmapp/ui/MainActivity.java
+++ app/src/main/java/com/lastfmapp/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lastfmapp.ui;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -35,15 +36,22 @@ public class MainActivity extends AppCompatActivity
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                  //  replaceFragmentWithAnimation(homeFragment, "home");
+                    transaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left);
                     transaction.replace(R.id.fragment_container, homeFragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
                     return true;
                 case R.id.navigation_dashboard:
+                   // replaceFragmentWithAnimation(searchFragment, "search");
+
+                    transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+                    //transaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left);
                     transaction.replace(R.id.fragment_container, searchFragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
@@ -52,6 +60,15 @@ public class MainActivity extends AppCompatActivity
             return false;
         }
     };
+
+    public void replaceFragmentWithAnimation(android.support.v4.app.Fragment fragment,  String tag){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        //transaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left);
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,5 +119,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onBackPressed() {
+       // super.onBackPressed();
+        finish();
     }
 }
